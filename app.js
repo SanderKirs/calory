45835e996dc79d7241f938a3b44669a878102ad9
SanderKirs <sander.kirs@khk.ee>
2022-03-06 17:52:39 +0200

    Created the UI with Materialize CSS.

---
+++ app.js
@@ -0,0 +1,13 @@
+const UIController = (function(){
+    let text = 'Hello World'
+    const changeText = function {
+        const element = document.querySelector('h1')
+        element.textContent = text
+    }
+    return {
+        callChangeText: function (){
+            changeText()
+            console.log(text)
+        }
+    }
+})();
